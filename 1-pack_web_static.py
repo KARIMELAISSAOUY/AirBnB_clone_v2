9bc312e4b3a945ee903b429807da0415dc360aee
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-09-08 05:52:12 -0700

    AirBnB_clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,16 @@ import os
 from datetime import datetime
 from fabric.api import local, runs_once
 
+
 @runs_once
 def do_pack():
     """Archives the static files."""
     if not os.path.exists("versions"):
         os.mkdir("versions")
-    
+
     cur_time = datetime.now()
     output = f"versions/web_static_{cur_time.strftime('%Y%m%d%H%M%S')}.tgz"
-    
+
     try:
         print(f"Packing web_static to {output}")
         local(f"tar -czf {output} web_static")
@@ -21,5 +22,5 @@ def do_pack():
     except Exception as e:
         print(f"An error occurred: {e}")
         output = None
-    
+
     return output
